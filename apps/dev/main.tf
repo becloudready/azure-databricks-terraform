2efbec81b9b52c110dee20ce9181a5aef71b5a18
Chandan Kumar <chandank@becloudready.com>
2025-07-06 11:20:07 -0400

    Added debug and hard coded filename

--- apps/dev/main.tf
+++ apps/dev/main.tf
@@ -6,3 +6,7 @@ module "nightly_serverless_job" {
   source = "../modules/databricks_jobs"
   notebook_file_path = "${path.module}/../../notebooks/nightly_sku_alert_job.py"
 }
+
+output "debug_notebook_path" {
+  value = var.notebook_file_path
+}
